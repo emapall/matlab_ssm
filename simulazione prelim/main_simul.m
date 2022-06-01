827d34db7eb21469ed4283f4e2857a4a9f271e39
emapall <ema.pallaoro@gmail.com>
2022-06-01 19:30:17 +0200

    polished graphics

--- simulazione prelim/main_simul.m
+++ simulazione prelim/main_simul.m
@@ -1,9 +1,8 @@
 clear all; close all;
 %% Variables initialization
-global rocketMass g d Lss Ls mu_din  h0 vxStop; %alpha0
-rocketMass = 10000; % 10 tons?
+global rocketMass g d Lss Ls mu_din  h0 dxMaxFoot; %alpha0
+rocketMass = 50000; % 10 tons?
 g = 10; %m/s^2
-vxStop = 1e-1;
 % geometric parameters
 d = 4; %m
 Ls = 8; % m, estimated from picture
@@ -11,37 +10,36 @@ h0 = 4; % m, distance of secondary strut joint from footpads plane
 a = 2; % m
 Lss = Ls-a;
 mu_din = 0.7;
-
+dxMaxFoot = 0.001;
 % initial conditions
 alpha0 = asin(h0/Ls); %estimated from picture
 v_vert0 = -3; %m/s
 % alphadot0 = v_vert0/Ls/cos(alpha0);
 %% damper
 global pIn xIn Aa Ab Kf Kp Lp_extended; 
-pIn = .5e4; % 2 atm
-xIn = 1; % m
-Aa = pi*.3^2;
-Ab = pi*.35^2;
+pIn = 1e5; % 2 atm
+xIn = 1.5; % m air chamber lenght initial
+Aa = pi*.15^2;
+Ab = pi*.20^2;
 Kf = 0;
-Kp = 1e4; % 0.5 atm per 1 m/s of stroke compression speed;
+Kp = 5e5; % 0.5 atm per 1 m/s of stroke compression speed;
 Lp_extended =sqrt(d^2+Lss^2+2*d*Lss*sin(alpha0));
 
 %% differential equation
-time_extremes = [0 2];
+time_extremes = [0 10];
 y0 = zeros(6,1);
 %y is a coloumn vector holding the state of the system, so composed:
 % 1 2 ) v_x/y rocket 3 4) x/y rocket
 % 5) alpha_dot 6) alpha 
 % 7) gamma_dot 8) gamma only if non-vertical
-y0 = [0; v_vert0; 0; 4;];
-opt = odeset('AbsTol',1e-3,'RelTol',1e-3);
+y0 = [0; v_vert0; 0; 4; 0];
+opt = odeset('AbsTol',1e-6,'RelTol',1e-6);
 [t,y] = ode45(@odeFunRocket3D,time_extremes,y0,opt);
 
 %% debugging and visual - re-calculating everything 
 N = length(t);
 Lp=zeros(N,1);
 alpha=Lp; alphadot=Lp; Fsx=Lp; Fsy=Lp; phi=Lp; Lpdot=Lp; Fp=Lp; pa=Lp; pb=pa; x=Lp; R=Lp;
-
 
 for i=1:N
     alpha(i) = asin(y(i,4)/Ls);
@@ -56,24 +54,44 @@ for i=1:N
     end
     end
 end
-    
-    
+xLeg = Ls*cos(alpha);
 
+%% graphics: 
+f1 = figure;
+sgtitle("Quantities vs time(s)");
+subplot(2,2,1);
+plot(t,y(:,4));
+title("Vertical position(m)");
 
+subplot(2,2,2);
+plot(t,R/rocketMass/g);
+title("Reaction acceleration(g's)");
 
-%%
-Lp =sqrt(d^2+Lss^2+2*d*Lss*sin(alpha));
-phi = acos((Lss^2-d^2-Lp.^2)./Lp/d/2); % cosine theorem: Lss^2 = d^2+Lp^2-2Lp*d*cos(phi)
-Lpdot = d*Lss*cos(alpha)./Lp.*alphadot;
-[Fp, pa, pb,x] = damperForce(Lp,Lpdot);
-R = (-Fp).*(sin(phi-alpha))./(...
-                    (cos(alpha)+sign(alphadot).*mu_din)*(1-(Ls-Lss)/Lss)...
-            );
+subplot(2,2,3);
+plot(t,pa/1e5);hold on;plot(t,pb/1e5);
+ylabel("press, atm");
+yyaxis right;
+ylabel("Piston stroke, m");
+title("<-Pressure and Piston stroke->");
+plot(t,x);
+title("Piston stroke (m)");
 
+subplot(2,2,4);
+plot(t,Fp);
+title("Piston Force");
 
+%% call to animation function:
 
-% FROM HERE one can call any visual debug function one might like
-
-
-% IMPORTANT NOTE: X LIMIT FOR WHICH COMPUTATION EXPLODES IS ~5 to 0.1 mm
-% (3-5 computation steps).
+%% graphics:
+% dt = diff(t);
+% for i=1:length(t)
+%     plot(nan,nan); hold on;
+%     xLeg = Ls*cos(alpha(i));
+%     dxFoot = y(i,5);
+% %     xLeg = 0;
+%     vxLeg = -Ls.*sin(alpha(i)).*alphadot(i)
+%     plot([xLeg xLeg-dxFoot],[0 -1],"*-");
+%     plot([xLeg+0 xLeg+vxLeg],[0 0],"g-o");
+%     xaxis([0 9]);
+%     hold off;pause(dt(i));
+% end
